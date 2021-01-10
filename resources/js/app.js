e3d71e65a073c970aff0f84d798b614bb6d1043c
thestoneoflapiz <thestoneoflapiz@gmail.com>
2021-01-10 19:06:44 +0800

    VueJS implemented --save

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import App from './app.vue';
+
+const app = new Vue({
+    el: "#app",
+    components: { App }
+});
---
+++ resources/js/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Event Calendar | Vue x Laravel</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {},
+    data() {
+        return{
+            event: "",
+            from: "",
+            to: ""
+        }
+    }
+}
+</script>
